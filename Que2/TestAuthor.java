91bef7997f56f4ead3ba20bee31247c6f2af02af
Raeshntr <47311673+Raeshntr@users.noreply.github.com>
2019-02-11 13:03:00 +0530

    Add files via upload

---
+++ Que2/TestAuthor.java
@@ -0,0 +1,17 @@
+package com.abc;
+
+public class TestAuthor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Author b1=new Author("ram","ram@gmail.com",'m');
+		System.out.println(b1);
+		b1.setEmail("ram@gmail.com");
+		System.out.println(b1);
+		System.out.println("name is:"+b1.getName());
+		System.out.println("gender is:"+b1.getGender());
+		System.out.println("email is:"+b1.getEmail());
+
+	}
+
+}
